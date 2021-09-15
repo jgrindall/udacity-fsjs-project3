00f446193a99440f8b7fef55bb603f9e666a295e
jgrindall <john.grindall@gmail.com>
2021-09-15 09:57:58 +0100

    new material design stuff

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -13,3 +13,27 @@ export class LoginComponent implements OnInit {
   }
 
 }
+
+
+//TODO
+
+import {PostDialogComponent} from '../post-dialog/post-dialog.component';
+import {MatDialog} from '@angular/material';
+
+// ... @Component ...
+export class DashboardComponent {
+  constructor(public auth: AuthService, public dialog: MatDialog, private dataService: DataService) {
+  }
+  // ... displayedColumns, dataSource, and deletePost ...
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(PostDialogComponent, {
+      width: '600px',
+      data: 'Add Post'
+    });
+    dialogRef.componentInstance.event.subscribe((result) => {
+      this.dataService.addPost(result.data);
+      this.dataSource = new PostDataSource(this.dataService);
+    });
+  }
+}
